ceac9db6b51ffeb675afdeca7e67376b4f7aaf7f
denglun0302 <2393751439@qq.com>
2019-11-02 08:40:12 +0800

    提交代码

---
+++ MapDemo/src/MapDemo.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class MapDemo {
+    public static void main(String[] args) {
+        Map<String,String> map = new HashMap<>();
+        System.out.println(map.size());
+        System.out.println(map.isEmpty());
+        System.out.println(map.get("作者"));
+        System.out.println(map.getOrDefault("作者", "佚名"));
+        System.out.println(map.containsKey("作者"));
+        System.out.println(map.containsValue("佚名"));
+        map.put("作者", "鲁迅");
+        map.put("标题", "狂人日记");
+        map.put("发表时间", "1918年");
+        System.out.println(map.size());
+        System.out.println(map.isEmpty());
+        System.out.println(map.get("作者"));
+        System.out.println(map.getOrDefault("作者", "佚名"));
+        System.out.println(map.containsKey("作者"));
+        System.out.println(map.containsValue("佚名"));
+    }
+}
